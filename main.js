fb1a1b6fc60c4e777cadd320495a786c0d74da6b
Piyushprabin <96835139+Piyushprabin@users.noreply.github.com>
2022-12-23 16:51:07 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,42 @@
+
+objectDetector= "";
+
+img = "";
+objects = [];
+status = "";
+
+function preload(){
+  img = loadImage('download(2).jpg');
+}
+
+
+function setup() {
+  canvas = createCanvas(640, 420);
+  canvas.center();
+  objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+  document.getElementById("status").innerHTML = "Status : Detecting Objects";
+}
+
+function modelLoaded() {
+  console.log("Model Loaded!")
+  status = true;
+  objectDetector.detect(img, gotResult);
+}
+
+function gotResult(error, results) {
+  if (error) {
+    console.log(error);
+  }
+  console.log(results);
+  objects = results;
+}
+
+
+function draw() {
+  image(img, 0, 0, 640, 420);
+
+      if(status != "")
+      {
+       
+      }
+}
